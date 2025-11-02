d860f9ef23e16be982aae9ac960c5f7a92c1df3a
Artem23112 <artemchernichuk@gmail.com>
2025-11-02 22:24:05 +0200

    Backend: Add cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,11 @@
 import * as trpcExpress from '@trpc/server/adapters/express'
+import cors from 'cors'
 import express from 'express'
 import { trpcRouter } from './trpc'
 
 const expressApp = express()
 
+expressApp.use(cors())
 expressApp.use('/trpc', trpcExpress.createExpressMiddleware({ router: trpcRouter }))
 
 expressApp.listen(3000, () => {
