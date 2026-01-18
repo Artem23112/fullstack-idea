d3b7a6c5584239e2ba17434f1f533b5b10756c2d
artem23112 <artemchernichuk@gmail.com>
2026-01-18 13:24:19 +0300

    backend: expand idea 1 description with more details

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -2,7 +2,12 @@ import { initTRPC } from '@trpc/server'
 const trpc = initTRPC.create()
 
 const ideas = [
-  { nick: 'cool-idea-nick-1', name: 'Idea 1', description: 'Description of idea 1...' },
+  {
+    nick: 'cool-idea-nick-1',
+    name: 'Idea 1',
+    description:
+      'An innovative concept that aims to revolutionize the way we approach problem-solving in the modern world. This idea focuses on leveraging emerging technologies to create efficient and sustainable solutions.',
+  },
   { nick: 'cool-idea-nick-2', name: 'Idea 2', description: 'Description of idea 2...' },
   { nick: 'cool-idea-nick-3', name: 'Idea 3', description: 'Description of idea 3...' },
   { nick: 'cool-idea-nick-4', name: 'Idea 4', description: 'Description of idea 4...' },
