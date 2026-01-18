11259c3ed3851f18329d6d45ee944bd19ea4cb43
artem23112 <artemchernichuk@gmail.com>
2026-01-18 19:21:45 +0200

    Common: Fix import order in main.tsx and index.ts

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -1,7 +1,8 @@
+import { QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import { App } from './App.tsx'
-import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from './lib/trpc.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
