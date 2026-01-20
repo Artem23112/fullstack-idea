bc4597f9314f246491d1d802e2cf68e733a95840
artem23112 <artemchernichuk@gmail.com>
2026-01-20 12:04:51 +0200

    Frontend: Rename AllIdeasPage to follow naming conventions
    Add links to individual idea pages

--- webapp/src/pages/all-ideas-page/AllIdeasPage.tsx
+++ webapp/src/pages/all-ideas-page/AllIdeasPage.tsx
@@ -1,5 +1,7 @@
+import { getViewIdeaRoute } from '@lib/routes'
 import { trpc } from '@lib/trpc'
 import { useQuery } from '@tanstack/react-query'
+import { Link } from 'react-router-dom'
 
 export const AllIdeasPage = () => {
   const { data } = useQuery(trpc.getIdeas.queryOptions())
@@ -9,7 +11,9 @@ export const AllIdeasPage = () => {
       <h1>Ideanick</h1>
       {data?.ideas.map((idea) => (
         <div key={idea.nick}>
-          <h2>{idea.name}</h2>
+          <h2>
+            <Link to={getViewIdeaRoute({ ideaNick: idea.nick })}>{idea.name}</Link>
+          </h2>
           <p>{idea.description}</p>
         </div>
       ))}
