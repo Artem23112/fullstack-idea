2474fbc11f744aa833d0fac21af7a59dc9dd013a
artem23112 <artemchernichuk@gmail.com>
2026-01-25 12:23:30 +0200

    Frontend: Replace App with RouterProvider for routing
    Add router import from react-router-dom

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -1,14 +1,16 @@
-import { QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
-import ReactDOM from 'react-dom/client'
 
-import { App } from './App.tsx'
+import { QueryClientProvider } from '@tanstack/react-query'
+import ReactDOM from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+
 import { queryClient } from './lib/trpc.ts'
+import { router } from './router.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </React.StrictMode>
 )
